b21bdbc4d75a9596a55f902e92084e60f26d342c
Sibyl666 <54271295+Sibyl666@users.noreply.github.com>
2020-07-24 01:08:55 +0300

    code cleanup

--- main.py
+++ main.py
@@ -4,29 +4,33 @@ import sys
 import time
 from PIL import Image
 
+thresh = 70
+TIMEOUT = .0416
+
+print("don't forget to add extension of video like mp4")
 videon_name = input("Video Name: ")
-print("don't forget to add extension of video")
+
 vidcap = cv2.VideoCapture(videon_name)
 success, image = vidcap.read()
 
-numpy.set_printoptions(threshold=sys.maxsize)
-thresh = 70
 
-TIMEOUT = .0416
+def fn(x):
+    if x > thresh:
+        return 255
+    return 0
+
+
 old_timestamp = time.time()
-
 while success:
-    
+
     if (time.time() - old_timestamp) > TIMEOUT:
         img = Image.fromarray(image)
         img.thumbnail((50, 50))
-        def fn(x): return 255 if x > thresh else 0
-        r = img.convert('L').point(fn, mode='1')
+        r = img.convert('L').point(fn, mode="1")
 
         pixels = r.load()
         image_size = r.size
 
-        row_count = 0
         star_string = ""
         for height in range(image_size[1]):
             for width in range(image_size[0]):
