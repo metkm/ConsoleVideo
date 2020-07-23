2f670baa11ead725fc8fc027a12ea7ab4ad568d2
Sibyl666 <54271295+Sibyl666@users.noreply.github.com>
2020-07-23 18:42:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,34 +5,40 @@ import time
 from PIL import Image
 
 videon_name = input("Video Name: ")
-print("don't forget to add extension of vide")
+print("don't forget to add extension of video")
 vidcap = cv2.VideoCapture(videon_name)
-
 success, image = vidcap.read()
+
 numpy.set_printoptions(threshold=sys.maxsize)
 thresh = 70
 
+TIMEOUT = .0416
+old_timestamp = time.time()
+
 while success:
-    img = Image.fromarray(image)
-    img.thumbnail((50, 50))
-    fn = lambda x: 255 if x > thresh else 0
-    r = img.convert('L').point(fn, mode='1')
+    
+    if (time.time() - old_timestamp) > TIMEOUT:
+        img = Image.fromarray(image)
+        img.thumbnail((50, 50))
+        def fn(x): return 255 if x > thresh else 0
+        r = img.convert('L').point(fn, mode='1')
 
-    pixels = r.load()
-    image_size = r.size
+        pixels = r.load()
+        image_size = r.size
 
-    row_count = 0
-    star_string = ""
-    for height in range(image_size[1]):
-        for width in range(image_size[0]):
-            pixel = pixels[width, height]
-            if not pixel == 255:
-                star_string += "*"
-                continue
-            else:
-                star_string += " "
-        star_string += "\n"
-    print(star_string)
-    time.sleep(0.0145)
+        row_count = 0
+        star_string = ""
+        for height in range(image_size[1]):
+            for width in range(image_size[0]):
+                pixel = pixels[width, height]
+                if not pixel == 255:
+                    star_string += "*"
+                    continue
+                else:
+                    star_string += " "
+            star_string += "\n"
 
-    success, image = vidcap.read()
+        old_timestamp = time.time()
+        print(star_string)
+
+        success, image = vidcap.read()
